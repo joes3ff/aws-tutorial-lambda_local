d1f8601334056e28facc7388a0fd4d65622fe415
Joe S <joesynnott90@gmail.com>
2025-03-28 14:29:23 +0000

    Updated to add Python version, reverted .js lambda to factory settings.

--- lambda/lambda.js
+++ lambda/lambda.js
@@ -3,7 +3,5 @@ exports.lambdaHandler = async (event, context) => {
     return event.data;
 };
 
-// demo push
-// demo push 2
 
 //sam local invoke -e ./lambda/lambda_event.json LambdaDemoFunction  
